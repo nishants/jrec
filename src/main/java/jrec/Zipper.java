4150b54f26ae76bd0f79abf59828b1ca8223700f
nishant <nishant.singh87@gmail.com>
2014-04-20 22:59:40 +0530

    Unzip a zip file

--- src/main/java/jrec/Zipper.java
+++ src/main/java/jrec/Zipper.java
@@ -3,15 +3,15 @@ package jrec;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 @Component
 public class Zipper {
+  private static final int BLOCK_SIZE = 4096;
+
   public void zipFiles(File inDir, File toFile) throws IOException {
     FileOutputStream fileIn = new FileOutputStream(toFile);
     ZipOutputStream zipOut = new ZipOutputStream(fileIn);
@@ -34,5 +34,58 @@ public class Zipper {
     zos.putNextEntry(zipentry);
     IOUtils.copy(new FileInputStream(file), zos);
     zos.closeEntry();
+  }
+
+  public void unZip(File zipFile, File toDir) throws IOException {
+    ZipInputStream input = null;
+    try {
+      input = readZip(zipFile);
+      unZip(input, toDir);
+    } finally {
+      if (input != null) input.close();
+    }
+  }
+
+  private static void unZip(ZipInputStream input, File toDir) throws IOException {
+    for (ZipEntry nextEntry = next(input); nextEntry != null; nextEntry = next(input)) {
+      if (nextEntry.isDirectory()) {
+        createDir(toDir, nextEntry.getName());
+      } else {
+        createFile(toDir, input, nextEntry.getName());
+      }
+    }
+  }
+
+  private static ZipEntry next(ZipInputStream zin) throws IOException {
+    return zin.getNextEntry();
+  }
+
+  private static void createFile(File toDir, ZipInputStream zin, String fileName) throws IOException {
+    String dir = dirOf(fileName);
+    if (dir != null) createDir(toDir, dir);
+    extractFile(zin, toDir, fileName);
+  }
+
+  private static ZipInputStream readZip(File zipFile) throws FileNotFoundException {
+    return new ZipInputStream(new FileInputStream(zipFile));
+  }
+
+  private static String dirOf(String name) {
+    int s = name.lastIndexOf(File.separatorChar);
+    return s == -1 ? null : name.substring(0, s);
+  }
+
+  private static void createDir(File toCreate, String path) {
+    File dir = new File(toCreate, path);
+    dir.mkdirs();
+  }
+
+  private static void extractFile(ZipInputStream in, File toDir, String name) throws IOException {
+    byte[] buffer = new byte[BLOCK_SIZE];
+    BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(toDir, name)));
+    int count = -1;
+    while ((count = in.read(buffer)) != -1)
+      out.write(buffer, 0, count);
+    out.close();
   }
 }
--- src/test/java/jrec/ZipperTest.java
+++ src/test/java/jrec/ZipperTest.java
@@ -13,11 +13,13 @@ public class ZipperTest {
 
   private String targetPath;
   private File outputFile;
+  private Zipper zipper;
 
   @Before
   public void setup() {
     targetPath = getClass().getResource("/fixtures/cassettes").getFile();
     outputFile = new File(tempFile(), "cassettes.zip");
+    zipper = new Zipper();
   }
 
   @Test
@@ -28,6 +30,30 @@ public class ZipperTest {
     zipper.zipFiles(target, outputFile);
 
     assertThat(outputFile.exists(), is(true));
+  }
+
+  @Test
+  public void shouldUnZipToDestination() throws IOException {
+    File zipFile = file(getClass().getResource("/fixtures/cassettes").getFile() + "/cassettes.zip");
+    File target = file(tempFile());
+
+    File expectedOutput = file(target, "cassettes/jrec/TestClassName/methodName.yaml");
+    expectedOutput.delete();
+
+    assertThat(expectedOutput.exists(), is(false));
+    assertThat(zipFile.exists(), is(true));
+
+    zipper.unZip(zipFile, target);
+
+    assertThat(expectedOutput.exists(), is(true));
+  }
+
+  private File file(File target, String name) {
+    return new File(target, name);
+  }
+
+  private File file(String file) {
+    return new File(file);
   }
 
   private String tempFile() {

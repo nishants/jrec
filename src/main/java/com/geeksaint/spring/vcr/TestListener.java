07c04fd88c80ab49992b99e3eed87a01c4c76d5a
Nishant <nishant.singh87@gmail.com>
2014-07-27 18:47:25 +0530

    removing spring.vcr package

--- src/main/java/com/geeksaint/spring/vcr/TestListener.java
+++
@@ -1,8 +0,0 @@
-package com.geeksaint.spring.vcr;
-
-public interface TestListener {
-  void beforeTestMethod(String testName);
-  void afterTestMethod(String testName);
-  void beforeTestClass();
-  void afterTestClass();
-}
--- src/test/java/com/geeksaint/spring/vcr/JRecRuntTimeTest.java
+++
@@ -1,4 +0,0 @@
-package com.geeksaint.spring.vcr;
-
-public class JRecRuntTimeTest {
-}
